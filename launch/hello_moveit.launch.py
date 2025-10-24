32c0cd423c1352037a651c1c8ace9ffb0b39ed02
Harsha Guda <harshavardhan.guda@gmail.com>
2025-10-24 17:31:15 +0200

    add example move to a given point with moiveit

---
+++ launch/hello_moveit.launch.py
@@ -0,0 +1,213 @@
+# Copyright (c) 2024 Franka Robotics GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import xacro
+import yaml
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, ExecuteProcess, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch import LaunchContext, LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import  LaunchConfiguration
+from launch_ros.actions import Node
+
+from launch.actions import (
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    IncludeLaunchDescription,
+    Shutdown
+)
+from launch.conditions import UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution
+)
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def get_robot_description(context: LaunchContext, arm_id, load_gripper, franka_hand):
+    arm_id_str = context.perform_substitution(arm_id)
+    load_gripper_str = context.perform_substitution(load_gripper)
+    franka_hand_str = context.perform_substitution(franka_hand)
+
+    franka_xacro_file = os.path.join(
+        get_package_share_directory('franka_description'),
+        'robots',
+        arm_id_str,
+        arm_id_str + '.urdf.xacro'
+    )
+
+    robot_description_config = xacro.process_file(
+        franka_xacro_file,
+        mappings={
+            'arm_id': arm_id_str,
+            'hand': load_gripper_str,
+            'ros2_control': 'true',
+            'gazebo': 'true',
+            'ee_id': franka_hand_str
+        }
+    )
+    robot_description = {'robot_description': robot_description_config.toxml()}
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='both',
+        parameters=[
+            robot_description,
+            {'use_sim_time': True}
+        ]
+    )
+
+    return [robot_state_publisher]
+
+
+
+def generate_launch_description():
+    # Configure ROS nodes for launch
+    arm_id_str = "fr3"
+    load_gripper_str = "true"
+    franka_hand_str = "franka_hand"
+    franka_xacro_file = os.path.join(
+        get_package_share_directory('franka_description'),
+        'robots',
+        arm_id_str,
+        arm_id_str + '.urdf.xacro'
+    )
+
+    robot_description_config = xacro.process_file(
+        franka_xacro_file,
+        mappings={
+            'arm_id': arm_id_str,
+            'hand': load_gripper_str,
+            'ros2_control': 'true',
+            'gazebo': 'true',
+            'ee_id': franka_hand_str
+        }
+    )
+
+    # planning_context
+    franka_xacro_file = os.path.join(
+        get_package_share_directory('franka_description'),
+        'robots', 'fr3', 'fr3.urdf.xacro'
+    )
+
+    robot_description_config = Command(
+        [FindExecutable(name='xacro'), ' ', franka_xacro_file, ' hand:=true',
+          ' ros2_control:=true', ' gazebo:=true', ' arm_id:=fr3', ' ee_id:=franka_hand'])
+
+    robot_description = {'robot_description': ParameterValue(
+        robot_description_config, value_type=str)}
+    franka_semantic_xacro_file = os.path.join(
+        get_package_share_directory('franka_description'),
+        'robots', 'fr3', 'fr3.srdf.xacro'
+    )
+
+    robot_description_semantic_config = Command(
+        [FindExecutable(name='xacro'), ' ',
+         franka_semantic_xacro_file, ' hand:=true']
+    )
+
+    robot_description_semantic = {'robot_description_semantic': ParameterValue(
+        robot_description_semantic_config, value_type=str)}
+
+    kinematics_yaml = load_yaml(
+        'franka_fr3_moveit_config', 'config/kinematics.yaml'
+    )
+
+    # Planning Functionality
+    ompl_planning_pipeline_config = {
+        'move_group': {
+            'planning_plugin': 'ompl_interface/OMPLPlanner',
+            'request_adapters': 'default_planner_request_adapters/AddTimeOptimalParameterization '
+                                'default_planner_request_adapters/ResolveConstraintFrames '
+                                'default_planner_request_adapters/FixWorkspaceBounds '
+                                'default_planner_request_adapters/FixStartStateBounds '
+                                'default_planner_request_adapters/FixStartStateCollision '
+                                'default_planner_request_adapters/FixStartStatePathConstraints',
+            'start_state_max_bounds_error': 0.1,
+        }
+    }
+    ompl_planning_yaml = load_yaml(
+        'franka_fr3_moveit_config', 'config/ompl_planning.yaml'
+    )
+    ompl_planning_pipeline_config['move_group'].update(ompl_planning_yaml)
+
+    # Trajectory Execution Functionality
+    moveit_simple_controllers_yaml = load_yaml(
+        'franka_fr3_moveit_config', 'config/fr3_controllers.yaml'
+    )
+    moveit_controllers = {
+        'moveit_simple_controller_manager': moveit_simple_controllers_yaml,
+        'moveit_controller_manager': 'moveit_simple_controller_manager'
+                                     '/MoveItSimpleControllerManager',
+    }
+
+    trajectory_execution = {
+        'moveit_manage_controllers': True,
+        'trajectory_execution.allowed_execution_duration_scaling': 1.2,
+        'trajectory_execution.allowed_goal_duration_margin': 0.5,
+        'trajectory_execution.allowed_start_tolerance': 0.01,
+    }
+
+    planning_scene_monitor_parameters = {
+        'publish_planning_scene': True,
+        'publish_geometry_updates': True,
+        'publish_state_updates': True,
+        'publish_transforms_updates': True,
+    }
+
+    # Hello MoveIt Node
+    hello_moveit = Node(
+        package='hello_moveit',
+        executable='hello_moveit',
+        namespace="",
+        output='screen',
+        parameters=[
+            robot_description,
+            robot_description_semantic,
+            kinematics_yaml,
+            ompl_planning_pipeline_config,
+            trajectory_execution,
+            moveit_controllers,
+            planning_scene_monitor_parameters,
+            {'use_sim_time': True}
+        ],
+    )
+
+    return LaunchDescription([hello_moveit])
